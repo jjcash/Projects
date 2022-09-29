63134cd67685be77191ea2228f3d182aa2a3f406
Guilherme Zampiron Daniel <clee.vprice@gmail.com>
2022-09-29 11:35:28 -0300

    11:34 - 29/09

--- prisma/prisma-postgres/src/controllers/CreateUserController.ts
+++ prisma/prisma-postgres/src/controllers/CreateUserController.ts
@@ -3,16 +3,15 @@ import { prismaClient } from "../database/prismaClient";
 
 export class CreateUserController {
     async handle(request: Request, response: Response) {
-        const { user_id, password, email, birth } = request.body;
+        const { user_id, password, email } = request.body;
 
         const user = await prismaClient.user.create({
             data: {
                 user_id,
                 password,
-                email,
-                birth,
+                email
             },
-        })
+        });
 
         return response.json(user);
     }
--- prisma/prisma-postgres/src/server.ts
+++ prisma/prisma-postgres/src/server.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(2022, () => console.log("Server is running on port 2022"));
+app.listen(4003, () => console.log("Server is running on port 4003"));

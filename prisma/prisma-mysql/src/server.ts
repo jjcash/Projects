0686892825d10f8f05a3adec22760f0740fcef89
Guilherme Zampiron Daniel <clee.vprice@gmail.com>
2022-10-25 07:24:01 -0300

    07:23 - 25/10

--- prisma/prisma-mysql/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { router } from "./routes";
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.listen(4003, () => console.log("Server is running on port 4003"));
